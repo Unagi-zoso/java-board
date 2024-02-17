1e6631b4ae64c362cb4a74dcfae56c24dbdbecb3
unagi-zoso <unagi.zoso@gmail.com>
2024-02-17 23:29:22 +0900

    #3 Feat: Spring Data Rest  어노테이션 추가
    단수한 CRUD API 를 간편하게 만들기 위해 추가하였다.

--- src/main/java/com/nightdiver/javaboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/nightdiver/javaboard/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.nightdiver.javaboard.repository;
 
 import com.nightdiver.javaboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
