667c13b9cb663f0033e9f5c22b6d40083f26e5ad
unagi-zoso <unagi.zoso@gmail.com>
2024-02-26 01:22:42 +0900

    #19 Feat: ArticleComment 엔티티의 service, dto, test 작성
    테스트 시 테스트 규모를 고려해 인스턴스 생성 엔티티를 따로  클래스를 만들어 전역화 시키지는 않았다.

---
+++ src/main/java/com/nightdiver/javaboard/service/ArticleCommentService.java
@@ -0,0 +1,33 @@
+package com.nightdiver.javaboard.service;
+
+import com.nightdiver.javaboard.dto.ArticleCommentDto;
+import com.nightdiver.javaboard.repository.ArticleCommentRepository;
+import com.nightdiver.javaboard.repository.ArticleRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleCommentService {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    @Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComments(Long articleId) {
+        return List.of();
+    }
+
+    public void saveArticleComment(ArticleCommentDto dto) {
+    }
+
+    public void updateArticleComment(ArticleCommentDto dto) {
+    }
+
+    public void deleteArticleComment(Long articleCommentId) {
+    }
+}
+
