fb2694a98bd8d91f927cdcee6f69f224fcf5492a
unagi-zoso <unagi.zoso@gmail.com>
2024-02-28 21:12:43 +0900

    #19 Feat: AuthConrollerTest @MockBean 필드 추가
    `ArticleController` 에 `ArticleService` 가 추가되며 `@WebMvcTest` 가 이뤄지는 `AuthControllerTest` 에 `ArticleService` 를 `@MockBean` 으로 추가해 다른 계층으로부터 독립적인 환경을 유지시켰다.

--- src/test/java/com/nightdiver/javaboard/controller/AuthControllerTest.java
+++ src/test/java/com/nightdiver/javaboard/controller/AuthControllerTest.java
@@ -5,10 +5,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.nightdiver.javaboard.config.SecurityConfig;
+import com.nightdiver.javaboard.service.ArticleService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,6 +25,8 @@ public class AuthControllerTest {
     public AuthControllerTest(@Autowired MockMvc mockMvc) {
         this.mockMvc = mockMvc;
     }
+
+    @MockBean ArticleService articleService;
 
     @DisplayName("[view][GET] 로그인 페이지 - 정상 호출")
     @Test
