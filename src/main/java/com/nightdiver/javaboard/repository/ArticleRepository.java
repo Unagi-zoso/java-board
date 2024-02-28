7ab06d4023a8b5656a66f4901ab48294e122b00f
unagi-zoso <unagi.zoso@gmail.com>
2024-02-28 20:41:10 +0900

    #19 Feat: Article 비즈니스 로직 구현
    테스트를 토대로 게시판 서비스에 필요한 비즈니스 로직 구현 완료

--- src/main/java/com/nightdiver/javaboard/repository/ArticleRepository.java
+++ src/main/java/com/nightdiver/javaboard/repository/ArticleRepository.java
@@ -18,7 +18,11 @@ public interface ArticleRepository extends
         QuerydslPredicateExecutor<Article>,
         QuerydslBinderCustomizer<QArticle> {
 
-    Page<Article> findByTitle(String title, Pageable pageable);
+    Page<Article> findByTitleContaining(String title, Pageable pageable);
+    Page<Article> findByContentContaining(String searchKeyword, Pageable pageable);
+    Page<Article> findByUserAccount_UserIdContaining(String searchKeyword, Pageable pageable);
+    Page<Article> findByUserAccount_NicknameContaining(String searchKeyword, Pageable pageable);
+    Page<Article> findByHashtag(String searchKeyword, Pageable pageable);
 
     @Override
     default void customize(QuerydslBindings bindings, QArticle root) {
